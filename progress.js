648e1cc4bd88d042333332ee27f1bbf9a3af48db
Jonas Almeida <jalmeida@mathbiol.org>
2016-01-19 07:37:52 -0500

    sorting RU data data option

--- progress.js
+++ progress.js
@@ -8,6 +8,16 @@ setTimeout(function(){
     vcfUi.appendChild(div)
     var h=new Date()
     h+='<p>Source files</p>'
+    
+    if(VCF.dir.ids.slice(-1)[0].match('^sra_data')){ // if this is Tomasz data, sort it accordingly
+        //x = x.slice(-1).concat(x.slice(0,-1))
+        VCF.dir.ids = VCF.dir.ids.slice(-1).concat(VCF.dir.ids.slice(0,-1))
+        VCF.dir.vcfs = VCF.dir.vcfs.slice(-1).concat(VCF.dir.vcfs.slice(0,-1))
+    }
+
+    VCF.dir.ids.forEach(function(fid){
+        h+='<li>'+fid+'</li>'
+    })
     div.innerHTML=h
 
 
